8de531824078960a81b0c90fd273c6a792015287
Joshua Ogunsola <joshua.ogunsola07@gmail.com>
2023-10-31 11:46:42 -0400

    progress page setup

--- src/app/progress/page.js
+++ src/app/progress/page.js
@@ -1,4 +1,6 @@
 "use client";
+import React from 'react'; 
+import './progress.css'; 
 import { useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -15,6 +17,14 @@ export default function () {
         return (
             <div className="container max-w-6xl pt-16 space-y-4">
                 <p className="text-3xl md:text-4xl font-bold">Track your progress</p>
+
+                <div class = "progress-board">
+                    <div>
+                        <h1 class= "metrics">Personal Records</h1>
+                        <h1 class= "metrics">Caloric Intake</h1>
+                        <h1 class ="metrics">Body Metrics</h1>
+                    </div>
+                </div>
             </div>
         );
     }
