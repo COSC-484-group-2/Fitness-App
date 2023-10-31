4de9838a45b0976bc40e2346ae81038b5d7de5a8
subilawal <olawal1@students.towson.edu>
2023-10-31 12:35:16 -0400

    About Us Page
    Added About Us page setup and implementation. Added Icon with white stroke to go in front of our black background.

--- src/components/main-nav.js
+++ src/components/main-nav.js
@@ -29,6 +29,9 @@ export function MainNav({ className, ...props }) {
                         <NavLink href="/workout" selected={pathname.includes("/workout")}>
                             Workout
                         </NavLink>
+                        <NavLink href="/about-us" selected={pathname.includes("/about-us")}>
+                            About Us
+                        </NavLink>
                     </>}
                     {status === "unauthenticated" && <NavLink href="/login" selected={pathname.includes("/login")}>
                         Login
