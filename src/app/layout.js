c9b3f85216616d5f91a7933f788670dca325674c
MATHEWMESFIN <mathewmesfin@gmail.com>
2023-11-01 23:18:41 -0400

    added content and styling to workout page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./style/globals.css";
 import { ClientProviders } from "@/components/client-providers";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
