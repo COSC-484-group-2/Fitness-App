2fcf2063c7c1345c5ccd788219c3d93d830c1155
Joshua Ogunsola <joshua.ogunsola07@gmail.com>
2023-11-02 11:28:15 -0400

    unifrom progress page

---
+++ src/app/personal-records/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+export default function () {
+    
+    return (
+        <div className="container max-w-6xl pt-16 space-y-4">
+            <p className="text-3xl md:text-4xl font-bold">What record did we break?</p>
+            <div className= "lifting">
+                <h1>HELLO Record breaker</h1>
+            </div>
+        </div>
+    );
+    
+}
